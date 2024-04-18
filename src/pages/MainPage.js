e303fe8c844387999d0584aca275af56e55ae707
baek009 <mjse9@naver.com>
2024-04-18 13:19:15 +0900

    docs : 6_Hooks

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Counter from "../components/Counter.js";
-
+import Counter from "../components/Counter";
+import CounterReducer from "../components/Counterreducer.js";
+import InputReducer from "../components/inputReducer.js";
 
 const MainPage = () => {
   return (
@@ -8,9 +9,23 @@ const MainPage = () => {
       <h1>Hooks study</h1>
       <div>
         <h2>useState</h2>
-        <h3>예시: 카운터앱</h3>
-        <Counter />      
-        </div>
+        <h3>예시: 카운터 앱</h3>
+        {/* <Counter /> */}
+      </div>
+      <div>
+        <h2>useEffect</h2>
+        <h3>예시: 카운터 앱</h3>
+        {/* <Counter /> */}
+      </div>
+      <div>
+        <h2>useReducer</h2>
+        <h3>예시: 리듀서 활용 카운터 앱</h3>
+        <CounterReducer />
+      </div>
+      <div>
+        <h3>예시: input 상태 관리하기</h3>
+        <InputReducer />
+      </div>
     </div>
   );
 };
