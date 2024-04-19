f3afa57ae83ae7c410b6b5af193f0ed8c4055866
baek009 <mjse9@naver.com>
2024-04-19 11:38:06 +0900

    dcos : Hooks 1차 정리

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,29 +1,34 @@
 import React from "react";
 import Counter from "../components/Counter";
-import CounterReducer from "../components/Counterreducer.js";
-import InputReducer from "../components/inputReducer.js";
+import CounterReducer from "../components/CounterReducer";
+import InputReducer from "../components/InputReducer";
 
 const MainPage = () => {
   return (
     <div>
       <h1>Hooks study</h1>
-      <div>
+      {/* <div>
         <h2>useState</h2>
         <h3>예시: 카운터 앱</h3>
-        {/* <Counter /> */}
-      </div>
-      <div>
+        <Counter />
+      </div> */}
+      {/* <div>
         <h2>useEffect</h2>
         <h3>예시: 카운터 앱</h3>
-        {/* <Counter /> */}
-      </div>
-      <div>
+        <Counter />
+      </div> */}
+      {/* <div>
         <h2>useReducer</h2>
         <h3>예시: 리듀서 활용 카운터 앱</h3>
         <CounterReducer />
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <h3>예시: input 상태 관리하기</h3>
+        <InputReducer />
+      </div> */}
+      <div>
+        <h2>useRef</h2>
+        <h3>예시: 평균값 계산</h3>
         <InputReducer />
       </div>
     </div>
