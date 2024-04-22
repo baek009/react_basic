63bd8d882a2a2b748f1170ea45a8456ed1c25d1a
baek009 <mjse9@naver.com>
2024-04-22 13:18:30 +0900

    docs : 리액트 라우터

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+
+      <Route path="/articles/" element={<Articles />}>
+        <Route index element={<Navigate replace to="1" />} />
+        <Route path=":id" element={<Article />} />
+      </Route>
     </Routes>
   );
 }
