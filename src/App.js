6c5f7b388b2e5694eeb64133b75c33a891813f39
baek009 <mjse9@naver.com>
2024-04-23 12:44:15 +0900

    docs : 컴포넌트스타일링

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import Articles from "./pages/Articles";
-import Article from "./pages/Article";
-import Layout from "./components/Layout";
-import NotFound from "./pages/NotFound";
+import ScssComponent from "./components/ScssComponent";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
-      </Route>
-
-      <Route path="/articles" element={<Articles />}>
-        <Route index element={<Navigate replace to="1" />} />
-        <Route path=":id" element={<Article />} />
-      </Route>
-
-      <Route path="/login" element={<Login />} />
-      <Route path="/mypage" element={<mypage />} />
-
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div>
+      <ScssComponent />
+    </div>
   );
 }
 
